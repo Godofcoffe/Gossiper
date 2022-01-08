46bc85336a971aacec0bdd0c0bda91b73316800e
Kevin G. Tertuliano <robotnyte@gmail.com>
2022-01-08 16:29:54 -0300

    fix: pequenas correções

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,9 @@
-let httpRequest;
+let httpRequest
+const parser = new DOMParser()
 const base_url = "https://haveibeenpwned.com/api/v3/breach/"
 const btn_busca = document.getElementById("busca")
 const event_busca = document.getElementById("busca").addEventListener("change", update)
 const r = document.getElementsByClassName('resultado-busca')[0]
-
 
 if (window.XMLHttpRequest) {
     httpRequest = new XMLHttpRequest()
@@ -12,25 +12,25 @@ if (window.XMLHttpRequest) {
 }
 
 httpRequest.onreadystatechange = _change
-btn_busca.value = ""
 
 function update(evt){
     let url = base_url + btn_busca.value
     httpRequest.open("GET", url)
     httpRequest.send()
+    btn_busca.value = ""
 }
 
 function _change(){
   let response
   r.style.display = "inherit"
+  r.style.color = "white";
   try {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
-        alert("teste")
         response = httpRequest.responseText
         const json = JSON.parse(response)
         r.getElementsByTagName("h3")[0].innerText = json["Name"]
-        r.getElementsByTagName("span")[0].innerText = json["Description"]
+        r.getElementsByTagName("span")[0].innerHTML = json["Description"]
       }
       else if (httpRequest.status === 404) {
         r.innerText = "Not Found!"
@@ -41,6 +41,6 @@ function _change(){
     }
   }
   catch( e ) {
-    alert('Caught Exception: ' + e.description)
+    location.reload()
   }
 }
